556f63b94c4969a519fd8243382ef0ba8e6e8e7a
WeiZheTseng <weizhetseng@gmail.com>
2022-08-22 00:24:06 +0800

    l

--- dist/assets/js/all.js
+++ dist/assets/js/all.js
@@ -1,6 +1,19 @@
 "use strict";
 
-$(function () {
-  console.log('Hello Bootstrap5');
+$('.reply').click(function (e) {
+  e.preventDefault();
+  $('.reply_content,.answer,.assignment_btn_group').toggleClass('d-none');
+});
+$('.cancel').click(function (e) {
+  e.preventDefault();
+  $('.reply_content,.answer,.assignment_btn_group').toggleClass('d-none');
+});
+$('.Verified').click(function (e) {
+  e.preventDefault();
+  $('.Verified,.Verified_button').toggleClass('d-none');
+});
+$('.Verified_button').click(function (e) {
+  e.preventDefault();
+  $('.Verified,.Verified_button').toggleClass('d-none');
 });
 //# sourceMappingURL=all.js.map
