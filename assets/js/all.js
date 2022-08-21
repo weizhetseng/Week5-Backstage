675bc3eb0d3bb941e039c7af768cb68e41656588
WeiZheTseng <weizhetseng@gmail.com>
2022-08-22 00:24:36 +0800

    Update 2022-08-21T16:24:34.452Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -8,4 +8,12 @@ $('.cancel').click(function (e) {
   e.preventDefault();
   $('.reply_content,.answer,.assignment_btn_group').toggleClass('d-none');
 });
+$('.Verified').click(function (e) {
+  e.preventDefault();
+  $('.Verified,.Verified_button').toggleClass('d-none');
+});
+$('.Verified_button').click(function (e) {
+  e.preventDefault();
+  $('.Verified,.Verified_button').toggleClass('d-none');
+});
 //# sourceMappingURL=all.js.map
